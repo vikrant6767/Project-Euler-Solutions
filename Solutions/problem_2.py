58ffcad881c76688e2a008b1a9efce54a469265e
Vikrant Kumar <vikrantpositive@gmail.com>
2019-11-17 22:24:45 +0530

    Problem 2 Solved.

--- Solutions/problem_2.py
+++ Solutions/problem_2.py
@@ -4,9 +4,9 @@
 
 a = 1
 b = 2
-sum = 1
-while(a + b <= 4000000):
-    if (a + b) % 2 != 0:
+sum = 2
+while(a + b < 4000000):
+    if (a + b) % 2 == 0:
         sum += a + b
     b = a + b
     a = b - a
